62948828aea88402271950163f41c0381e62cd79
Christopher Morrrison <morriscb@uw.edu>
2018-04-06 12:03:29 -0700

    Modify names and namespace.

--- pz/calibrate/__init__.py
+++ pz/calibrate/__init__.py
@@ -1,4 +1,4 @@
 # Make sure any stages you want to use in a pipeline
 # are imported here.
 from ceci import PipelineStage
-from .pz_calibrateStage1 import pz_calibrateStage1
+from .calibrate import PZCalibrate
